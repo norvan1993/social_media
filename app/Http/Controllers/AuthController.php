cfa8f5cc3f3141b2dbc677b22df005c048c5d12c
norvan1993 <norvanwahi@gmail.com>
2019-05-10 12:44:26 +0300

    dailycommit 10-5-2019

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,14 +7,54 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    /**********************************************************
+     * login
+     **********************************************************/
     public function login(Request $request)
     {
+        $http = new \GuzzleHttp\Client;
+        //try to login
+        try {
+            $response = $http->post('http://carmeer.com/oauth/token', ['form_params' => [
+                'grant_type' => 'password',
+                'client_id' => 2,
+                'client_secret' => '7LhrUhAxOIHy4LK5NV7i3aCJgXdIYgmkFdYE5KgB',
+                'username' => $request->email,
+                'password' => $request->password
+            ]]);
+            //return token on success
+            return $response->getBody();
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            //return error message on error
+            if ($e->getCode() == 400) {
+                return response()->json('invalid Request', $e->getCode());
+            } else if ($e->getCode() == 401) {
+                return response()->json('your credential are incorrect', $e->getCode());
+            }
+            return response()->json('something went wrong on server', $e->getCode());
+        }
+    }
+    /**********************************************************
+     * register
+     *********************************************************/
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $input = $request->only(['name', 'email']);
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
+        //login
         $http = new \GuzzleHttp\Client;
         try {
             $response = $http->post('http://carmeer.com/oauth/token', ['form_params' => [
                 'grant_type' => 'password',
                 'client_id' => 2,
-                'client_secret' => 'W6F6uuNLRuuXiWQlM6iSDYsNtpRbe6hSNI5DEs0j',
+                'client_secret' => '7LhrUhAxOIHy4LK5NV7i3aCJgXdIYgmkFdYE5KgB',
                 'username' => $request->email,
                 'password' => $request->password
             ]]);
@@ -28,15 +68,9 @@ class AuthController extends Controller
             return response()->json('something went wrong on server', $e->getCode());
         }
     }
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-
-        ]);
-    }
+    /*************************************************************************
+     * logout
+     *************************************************************************/
     public function logout()
     {
         auth()->user()->tokens->each(function ($token, $key) {
